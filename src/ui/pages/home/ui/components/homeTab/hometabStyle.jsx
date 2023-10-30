2a13023e611ff413a42c4297709083ab0e66949f
Oat Sarayut <55384514+ORPGCLUP@users.noreply.github.com>
2023-10-30 21:30:38 +0700

    fix css of home page and navbar
    fix css of home page and navbar

---
+++ src/ui/pages/home/ui/components/homeTab/hometabStyle.jsx
@@ -0,0 +1,66 @@
+import { makeStyles } from '@mui/styles';
+function addImportant(styles) {
+  return Object.fromEntries(
+    Object.entries(styles).map(([key, value]) => [key, `${value} !important`])
+  );
+}
+const useStyles = makeStyles({
+
+  cover: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start', 
+  },
+  container: {
+    width: '100%', 
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch', 
+    
+  },
+  tabContainer: { 
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    borderColor: "divider",
+    borderBottomLeftRadius: "15px",
+    borderBottomRightRadius: "15px",
+    backgroundColor: "#ffffff",
+    '& .MuiTabs-flexContainer':{
+      display: 'flex',
+      justifyContent: 'space-evenly',
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    width: '100%',
+  },
+
+  containerInfo:{
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch', 
+    '@media (min-width: 601px) and (max-width: 1024px)': { 
+      width: '70%',
+    },
+    '@media (min-width: 1025px)': { 
+      width: '60%',
+    },
+  },
+  centerTab: addImportant({
+    flex: 1,
+    width: '50%',
+    color: 'black', 
+  }),
+  typoStyle: {
+    fontFamily: "Inter",
+    fontWeight: "700",
+    fontSize: "20px",
+  },
+})
+
+export default useStyles
--- src/ui/pages/home/ui/components/homeTab/hometap.jsx
+++ src/ui/pages/home/ui/components/homeTab/hometap.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Tabs, Tab, TabList, Typography, Box } from "@mui/material";
-import useStyles from "./homeTab";
+import useStyles from "./hometabStyle";
 
 import CreateBill from "../createBill";
 import Bill from "../bill";
@@ -28,46 +28,53 @@ function HomeTab() {
     setValue(newValue);
   };
   return (
-    <Box className={classes.conainer}>
-      <Box
-        className={classes.tabContainer}
-      >
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          sx={{ padding: "0 30px" }}
-          TabIndicatorProps={{
-            sx: { background: "#FFB53B" },
-          }}
-        >
-          <Tab
-            label="Homepage"
-            className={classes.centerTab}
-            {...a11yProps(0)}
-          />
-          <Tab
-            label="History"
-            className={classes.centerTab}
-            {...a11yProps(1)}
-          />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <Box>
-          <CreateBill />
-          <Typography className={classes.typoStyle}>
-            Pending bills
-          </Typography>
-          <Box mt={2}>
-            <Bill />
-            <Bill />
-            <Bill />
+    <Box className={classes.cover}>
+      <Box className={classes.container}>
+        <Box className={classes.tabContainer}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            className={classes.tabsStyle}
+            sx={{ padding: "0 30px" }}
+            TabIndicatorProps={{
+              sx: { 
+                background: "#FFB53B" ,        
+              },
+            }}
+          >
+            <Tab
+              label="Homepage"
+              className={classes.centerTab}
+              {...a11yProps(0)}
+            />
+            <Tab
+              label="History"
+              className={classes.centerTab}
+              {...a11yProps(1)}
+            />
+          </Tabs>
+        </Box>
+        <Box className={classes.cover}>
+          <Box className={classes.containerInfo}>
+            <TabPanel value={value} index={0}>
+              <Box>
+                <CreateBill />
+                <Typography className={classes.typoStyle}>
+                  Pending bills
+                </Typography>
+                <Box mt={2}>
+                  <Bill />
+                  <Bill />
+                  <Bill />
+                </Box>
+              </Box>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              Item two
+            </TabPanel>
           </Box>
         </Box>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item two
-      </TabPanel>
+      </Box>
     </Box>
   );
 }
