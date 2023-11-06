15a22f46332a67706935be330275fc2b5277fdd6
Watsapol Samittivate <97512569+MarkMPW@users.noreply.github.com>
2023-11-06 23:42:12 +0700

    create bank/promptpay buttons

--- frontend/src/ui/pages/splitingBill/components/bottomBar.jsx
+++ frontend/src/ui/pages/splitingBill/components/bottomBar.jsx
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from "react";
 import CircleIcon from "@mui/icons-material/Circle";
 import CreateIcon from "@mui/icons-material/Create";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
-import useStyles from '../style/bottomBarStyle'
+import useStyles from "../style/bottomBarStyle";
 
 import { Box, Typography, Button } from "@mui/material";
 
@@ -12,17 +13,19 @@ function BottomBar({ itemList }) {
   const [isBankAcc, setIsBankAcc] = useState(true);
   const [isPromptPay, setIsPromptPay] = useState(false);
 
-  const handleChangeIsProptPay = () => {
+  const handleChangeIsProptPay = (e) => {
+    e.preventDefault();
     setIsPromptPay(true);
     setIsBankAcc(false);
   };
 
-  const handleChangeBackAss = () => {
+  const handleChangeBackAss = (e) => {
+    e.preventDefault();
     setIsBankAcc(true);
     setIsPromptPay(false);
   };
 
-  const classes = useStyles()
+  const classes = useStyles({ isBankAcc, isPromptPay });
   return (
     <Box className={classes.cover}>
       <Box className={classes.container}>
@@ -31,57 +34,59 @@ function BottomBar({ itemList }) {
             <Typography variant="h4">Total</Typography>
             <Typography variant="h4">{itemList?.totalCost} ฿</Typography>
           </Box>
-          <Box
-            sx={{
-              display: 'flex',
-              marginTop: '20px',
-              justifyContent: 'space-between'
-            }}
-          >
+          <Box className={classes.positionTwoPaymentButton}>
             <Button
+              variant="contained"
               onClick={handleChangeBackAss}
-              sx={{
-                backgroundColor: '#FFB53B',
-                minWidth: '165px',
-                color: 'black'
-              }}
+              className={classes.positionBankAccButton}
             >
               Banck account
             </Button>
             <Button
-            onClick={handleChangeIsProptPay}
-              sx={{
-                backgroundColor: '#FFB53B',
-                minWidth: '150px',
-                color: 'black',
-              }}
+              variant="contained"
+              onClick={handleChangeIsProptPay}
+              className={classes.positionProptPayButton}
             >
               PromtPay
             </Button>
           </Box>
           {isBankAcc ? (
-            <Box className={classes.boxAccount}>
-                <Box sx={{ display: "flex" }}>
-                  <CircleIcon sx={{ fontSize: "40px" }} />
-                  <Box ml={1}>
-                    <Typography variant="h5">Account Name</Typography>
-                    <Typography variant="h6">Bank Name, Account Number</Typography>
-                  </Box>
-                </Box>
-              <CreateIcon />
-            </Box>
-          ): (
-            <Box className={classes.boxAccount}>
-                <Box sx={{ display: "flex" }}>
-                  <CircleIcon sx={{ fontSize: "40px" }} />
-                  <Box ml={1}>
-                    <Typography variant="h5">Account Name</Typography>
-                    <Typography variant="h6">Phone number</Typography>
-                  </Box>
-                </Box>
-              <CreateIcon />
-            </Box>
-          ) }
+            <Button className={classes.addBacnkAccButton}>
+              <Typography color="#000" variant="h5">
+                Add bank account details
+              </Typography>
+              <AddCircleIcon sx={{ color: "#545454" }} />
+            </Button>
+          ) : (
+            // <Box className={classes.boxAccount}>
+            //     <Box sx={{ display: "flex" }}>
+            //       <CircleIcon sx={{ fontSize: "40px" }} />
+            //       <Box ml={1}>
+            //         <Typography variant="h5">Account Name</Typography>
+            //         <Typography variant="h6">Bank Name, Account Number</Typography>
+            //       </Box>
+            //     </Box>
+            //   <CreateIcon />
+            // </Box>
+            // code above will later use as component
+            <Button className={classes.addPromptPayButton}>
+              <Typography color="#000" variant="h5">
+                Add PromptPay details
+              </Typography>
+              <AddCircleIcon sx={{ color: "#545454" }} />
+            </Button>
+            // <Box className={classes.boxAccount}>
+            //   <Box sx={{ display: "flex" }}>
+            //     <CircleIcon sx={{ fontSize: "40px" }} />
+            //     <Box ml={1}>
+            //       <Typography variant="h5">Account Name</Typography>
+            //       <Typography variant="h6">Phone number</Typography>
+            //     </Box>
+            //   </Box>
+            //   <CreateIcon />
+            // </Box>
+            // code above will later use as component
+          )}
           <Button
             fullWidth
             className={classes.positionBotton}
